0ed56d5d8a7ec2ebb9883b364dcc606998186c7f
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-04-03 13:47:15 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
-
 });
 
 var port = 81;
